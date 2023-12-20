b579dea0cdc89efc8b0e29737197073b488dd520
Müşerref Selçuk Özdemir <72154925+SelcukOzdemir23@users.noreply.github.com>
2023-12-20 11:11:15 +0300

    gradio_son

--- gradio_dene.py
+++ gradio_dene.py
@@ -39,12 +39,15 @@ def classify_image(pil_image):
     class_names = list(waste_labels.values())
     probabilities = prediction[0]
 
+    print(class_names)
+    print(probabilities)
    
     plt.bar(class_names, probabilities, color='blue')
     plt.xlabel('Waste Classes')
     plt.ylabel('Probability')
     plt.title('Prediction Probabilities')
     plt.savefig('prediction_plot.png')
+    plt.close()
 
     
     output_text = f"Predicted Class: {predicted_class_name}, Confidence: {confidence:.4f}\n"
@@ -54,7 +57,15 @@ def classify_image(pil_image):
     return output_text, 'prediction_plot.png'
 
 
-iface = gr.Interface(fn=classify_image, inputs="image", outputs=["text", "image"],live=True)
+iface = gr.Interface(fn=classify_image, 
+                     inputs="image", 
+                     outputs=["text", "image"],
+                     # examples=["L9_1b95a3808073c0edad3454d1dedf3dcc.jpg","L6_0a171beb21a6f4d6fef31f8ccb400eae.jpg","L2_00a6b5e9806a8b072b98fdeacb3f45b5.jpg","L4_0b02898e9d31954dd5378e0ffbdb9a41.jpg"],
+                     title= "SEM IMAGES CLASSIFICATION",
+                     description= "Fibres, Nanowires, Particles, Powder SEM görüntülerini sınıflandıran model arayüzü",
+                     theme=gr.themes.Soft(),
+                     live=True)
 
 
 iface.launch()
+
